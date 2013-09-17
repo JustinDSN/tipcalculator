7b2b825b1eba969cc7b168e7d4c719f09f7712ec
Justin Steffen <JustinDSN@gmail.com>
2013-09-17 16:53:33 -0700

    Initial commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Justin Steffen on 9/17/13.
+//  Copyright (c) 2013 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
